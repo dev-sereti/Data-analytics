bb9df101a6799bc26d05c337d776250b4ff47d5a
dev-sereti <devsereti@gmail.com>
2025-12-08 16:13:08 +0300

    Pandas - Cleaning Data

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
-data = pd.read_csv('C:\Users\VF3535\Documents\Analytics\pandas\data.csv')
+data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
+print(data)
