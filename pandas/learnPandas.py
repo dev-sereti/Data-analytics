c435cc91d2873d30ef9303d2584dc9b8a7ac3f36
dev-sereti <devsereti@gmail.com>
2025-12-08 16:24:25 +0300

    Pandas - Cleaning Data - Remove Rows

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -4,3 +4,7 @@ import pandas as pd
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 data.dropna(inplace=True)
 print(data.to_string)
+
+#Replace Empty Values
+
+data.fillna(130, inplace=True)
