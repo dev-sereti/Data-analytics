d9b7483ecda3b81353fbe55d86558cefe9978953
dev-sereti <devsereti@gmail.com>
2025-12-08 15:41:26 +0300

    Load Files Into a DataFrame

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-#Series (1D)
-numberPlate = ["KCA203J","KDA253U","KBC678D"]
+# #Series (1D)
+# numberPlate = ["KCA203J","KDA253U","KBC678D"]
 
-availablePlates = pd.Series(numberPlate, index= ["1","2","3"])
-#print (availablePlates[2])
+# availablePlates = pd.Series(numberPlate, index= ["1","2","3"])
+# #print (availablePlates[2])
 
 
-#DataFrames (2D)
-dataset = {
-    'carModels': ["Ford","BMW","Ford"],
-    'plateNumbers':["KCA203J","KDA253U","KBC678D"]
-}
-carData = pd.DataFrame(dataset, index = ["1","2","3"])
-# print(carData)
-print(carData.loc[3])
+# #DataFrames (2D)
+# dataset = {
+#     'carModels': ["Ford","BMW","Ford"],
+#     'plateNumbers':["KCA203J","KDA253U","KBC678D"]
+# }
+# carData = pd.DataFrame(dataset, index = ["1","2","3"])
+# # print(carData)
+# print(carData.loc[3])
 
 
