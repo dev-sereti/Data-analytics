12b8eb8f6d95e4b4943247ac70a6786e4da4c0ed
dev-sereti <devsereti@gmail.com>
2025-12-08 15:22:11 +0300

    Series in Panda

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+#Series (1D)
+numberPlate = ["KCA203J","KDA253U","KBC678D"]
 
-dataset = {
-    'carModels': ["Ford","BMW","Ford"],
-    'numberPlate':["KCA203J","KDA253U","KBC678D"]
-}
 
-carData = pd.DataFrame(dataset)
-print(carData)
+# dataset = {
+#     'carModels': ["Ford","BMW","Ford"],
+#     'numberPlate':["KCA203J","KDA253U","KBC678D"]
+# }
+
+# carData = pd.DataFrame(dataset)
+# print(carData)
