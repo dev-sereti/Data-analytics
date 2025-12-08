1521830c325234298146171caab74a292703fb74
dev-sereti <devsereti@gmail.com>
2025-12-08 15:13:58 +0300

    Python dictionary to pandas DataFrame

--- pandas/pandas.py
+++ pandas/pandas.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
+#Python dictionary
+dataset = {
+    'carModels': ["Ford","BMW","Ford"],
+    'numberPlate':["KCA203J","KDA253U","KBC678D"]
+}
 
-
+carData = pd.DataFrame(dataset)
+print(carData)
