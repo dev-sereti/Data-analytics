063c849defba270ad15021697f7f134e4772d2e2
dev-sereti <devsereti@gmail.com>
2025-12-08 15:35:47 +0300

    DataFrames in Panda

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -4,12 +4,13 @@ import pandas as pd
 numberPlate = ["KCA203J","KDA253U","KBC678D"]
 
 availablePlates = pd.Series(numberPlate, index= ["1","2","3"])
-print (availablePlates[2])
+#print (availablePlates[2])
 
-# dataset = {
-#     'carModels': ["Ford","BMW","Ford"],
-#     'plateNumbers':["KCA203J","KDA253U","KBC678D"]
-# }
 
-# carData = pd.DataFrame(dataset)
-# print(carData)
+#DataFrames (2D)
+dataset = {
+    'carModels': ["Ford","BMW","Ford"],
+    'plateNumbers':["KCA203J","KDA253U","KBC678D"]
+}
+carData = pd.DataFrame(dataset, index = ["1","2","3"])
+print(carData)
