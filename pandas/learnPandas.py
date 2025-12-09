1155fb1f4e1e34786e137a160873f9f23af47e17
dev-sereti <devsereti@gmail.com>
2025-12-09 09:56:51 +0300

    plotting

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 
-data['Date'] = pd.to_datetime(data['Date'], format='mixed')
+# parse Date robustly (coerce invalid formats to NaT)
+data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
 
-# print(data.corr(numeric_only=True))
+# set Date as index so time series plots look correct
+data = data.set_index('Date')
 
-data.plot()
-data.plot(x='Date', y='Calories')
+# example plot
+ax = data['Calories'].plot(title='Calories over time', xlabel='Date', ylabel='Calories')
+
+# show the plot window (important in scripts)
+plt.show()
