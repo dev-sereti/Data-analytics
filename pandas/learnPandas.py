acaf591750787ee14da71f44fd1615c1407438f2
dev-sereti <devsereti@gmail.com>
2025-12-09 08:03:47 +0300

    Replace values Using Mean, Median, or Mode

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -3,3 +3,5 @@ import pandas as pd
 # Remove Rows
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 
+newData = data["Calories"].mean()
+data.fillna({"Calories":newData}, inplace = True)
