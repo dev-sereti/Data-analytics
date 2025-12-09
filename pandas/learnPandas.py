b0537f3e0930247af77d5733ed2f39ef816db425
dev-sereti <devsereti@gmail.com>
2025-12-09 10:03:38 +0300

    NumPy

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,16 +1,1 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
-
-# parse Date robustly
-data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
-
-# set Date as index so time series plots look correct
-data = data.set_index('Date')
-
-# plot
-ax = data['Calories'].plot(title='Calories over time', xlabel='Date', ylabel='Calories')
-
-# show the plot window
-plt.show()
+#NumPy 
