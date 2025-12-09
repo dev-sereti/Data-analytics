785ecd723832eeb558e3d312014363472c1e5f83
dev-sereti <devsereti@gmail.com>
2025-12-09 08:04:34 +0300

    Mean

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -3,5 +3,8 @@ import pandas as pd
 # Remove Rows
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 
+#Mean
 newData = data["Calories"].mean()
 data.fillna({"Calories":newData}, inplace = True)
+
+#Median
