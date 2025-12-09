327f5b1013bdb2606c8e7f0c3a515b4c77eaaf7e
dev-sereti <devsereti@gmail.com>
2025-12-09 08:26:13 +0300

    Convert Column Type from String to Datetime Format in Pandas Dataframe

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
-df = pd.DataFrame({
+data = pd.DataFrame({
     'Date': ['11/8/2011', '04/23/2008', '10/2/2019'],
     'Event': ['Music', 'Poetry', 'Theatre'],
     'Cost': [10000, 5000, 15000]
 })
 print("Before Conversion:")
-print(df.info())
+print(data.info())
 # df['Date'] = pd.to_datetime(df['Date'])
 
 # print("\nAfter Conversion:")
