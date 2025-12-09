226dc4f5cadabb35f20f5bc8aa68812c2ce9636d
dev-sereti <devsereti@gmail.com>
2025-12-09 09:30:55 +0300

    Convert the date column to datetime

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
-
+data.columns()
 # Convert date column to datetime
-data['date'] = pd.to_datetime(data['date'])
+# data['date'] = pd.to_datetime(data['date'])
 # Pandas - Data Correlations
-data.corr()
+# data.corr()
