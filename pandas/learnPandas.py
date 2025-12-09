7e6639fbf081e44d4259b0f6332ec654f61d544d
dev-sereti <devsereti@gmail.com>
2025-12-09 08:31:11 +0300

    Converting from 'yymmdd' Format

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 # data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
-data = pd.DataFrame({
-    'Date': ['11/8/2011', '04/23/2008', '10/2/2019'],
-    'Event': ['Music', 'Poetry', 'Theatre'],
-    'Cost': [10000, 5000, 15000]
-})
-print("Before Conversion:")
-print(data.info())
-data['Date'] = pd.to_datetime(data['Date'])
+# data = pd.DataFrame({
+#     'Date': ['11/8/2011', '04/23/2008', '10/2/2019'],
+#     'Event': ['Music', 'Poetry', 'Theatre'],
+#     'Cost': [10000, 5000, 15000]
+# })
+# print("Before Conversion:")
+# print(data.info())
+# data['Date'] = pd.to_datetime(data['Date'])
 
-print("\nAfter Conversion:")
-print(data.info())
+# print("\nAfter Conversion:")
+# print(data.info())
 
 
 #Converting from 'yymmdd' Format
