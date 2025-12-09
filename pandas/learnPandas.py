51366707511826aec05e93b7829efcf6328e40da
dev-sereti <devsereti@gmail.com>
2025-12-09 09:12:28 +0300

    Replacing Values

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,27 +1,5 @@
 import pandas as pd
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
-#Replacing Values
-data.loc[7,'Duration'] = 45
-# data = pd.DataFrame({
-#     'Date': ['11/8/2011', '04/23/2008', '10/2/2019'],
-#     'Event': ['Music', 'Poetry', 'Theatre'],
-#     'Cost': [10000, 5000, 15000]
-# })
-# print("Before Conversion:")
-# print(data.info())
-# data['Date'] = pd.to_datetime(data['Date'])
-
-# print("\nAfter Conversion:")
-# print(data.info())
 
 
-#Converting from 'yymmdd' Format
-# patientData = pd.DataFrame({
-#     'Date': ['200712', '200714', '200716', '200719'],
-#     'Patients': [50000, 51000, 51500, 53000]
-# })
-
-# print(patientData.dtypes)
-# patientData['Date'] = pd.to_datetime(patientData['Date'], format='%y%m%d')
-# print("After Conversion:\n")
-# print(patientData.dtypes)
+#Removing duplicates
