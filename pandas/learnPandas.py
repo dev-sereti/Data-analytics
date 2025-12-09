8daf851d3ffc298c9054358a025edd9d2f9706f4
dev-sereti <devsereti@gmail.com>
2025-12-09 08:09:35 +0300

    Pandas - Cleaning Data of Wrong Format

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -3,14 +3,17 @@ import pandas as pd
 # Remove Rows
 data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 
-#Mean
-newData = data["Calories"].mean()
-data.fillna({"Calories":newData}, inplace = True)
+# #Mean
+# newData = data["Calories"].mean()
+# data.fillna({"Calories":newData}, inplace = True)
 
-#Median
-newData = data["Calories"].median()
-data.fillna({"Calories":newData}, inplace = True)
+# #Median
+# newData = data["Calories"].median()
+# data.fillna({"Calories":newData}, inplace = True)
 
-# Mode
-newData = data["Calories"].mode()
-data.fillna({"Calories":newData}, inplace = True)
+# # Mode
+# newData = data["Calories"].mode()
+# data.fillna({"Calories":newData}, inplace = True)
+
+
+# Pandas - Cleaning Data of Wrong Format
