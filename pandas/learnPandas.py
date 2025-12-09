aef8a98177db35395b07a70e5e3f201c6af71085
dev-sereti <devsereti@gmail.com>
2025-12-09 09:53:04 +0300

    plotting

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -4,4 +4,7 @@ data = pd.read_csv(r"C:\Users\VF3535\Documents\Analytics\pandas\data.csv")
 
 data['Date'] = pd.to_datetime(data['Date'], format='mixed')
 
-print(data.corr(numeric_only=True))
+# print(data.corr(numeric_only=True))
+
+data.plot()
+data.plot(x='Date', y='Calories')
