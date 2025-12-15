4f87ab0f1fee1d89ed2ae191b1daf6e296c79796
dev-sereti <devsereti@gmail.com>
2025-12-15 16:45:12 +0300

    Handle missing ages: fill with median by gender

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -20,4 +20,8 @@ data_orders = data.drop_duplicates(subset=["order_id"])
 
 
 # Handle missing ages: fill with median by gender
-data_orders["customer_age"] = data_orders.groupby("gender")["customer_age"].transform(lambda s:s.fillna(s.median))
+data_orders.loc[:, "customer_age"] = (
+    data_orders
+    .groupby("gender")["customer_age"]
+    .transform(lambda s: s.fillna(s.median()))
+)
