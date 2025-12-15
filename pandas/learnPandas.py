f9470cf820e56bb70f1904e4a5361c006a5e9377
dev-sereti <devsereti@gmail.com>
2025-12-15 15:26:35 +0300

    print shape

--- pandas/learnPandas.py
+++ pandas/learnPandas.py
@@ -1,7 +1,10 @@
+# import os
+# print(os.getcwd())
 import pandas as pd
 
-data = pd.read_csv("sample_retail_transactions_dataset.csv", 
-parse_dates= ["order_dates","ship_date","signup_date","last_login"]
+data = pd.read_csv(
+    "sample_retail_transactions_dataset.csv",
+    parse_dates=["order_dates", "ship_date", "signup_date", "last_login"]
 )
 
 print(data.shape)
