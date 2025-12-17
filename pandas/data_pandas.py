6a7f7a9e27c698f570247579b0a597af20cafb64
dev-sereti <devsereti@gmail.com>
2025-12-17 12:01:45 +0300

    Load data

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 # Load data
 
+data = pd.re
+
+
 # data = pd.read_csv(
 #     r"C:\Users\Kelvin\Documents\Sereti\Data-analytics\pandas\",
 #     parse_dates=["order_date", "ship_date", "signup_date", "last_login"]
