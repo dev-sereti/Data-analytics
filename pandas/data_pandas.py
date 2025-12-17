5e073f8fdf3c5e124345eef2abbd872b2eb0c5c6
dev-sereti <devsereti@gmail.com>
2025-12-17 11:59:03 +0300

    Check path

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -1,8 +1,8 @@
-# import os
-# print(os.getcwd())
+import os
+print(os.getcwd())
 import pandas as pd
 
-data = pd.read_csv(
-    r"C:\Users\Kelvin\Documents\Sereti\Data-analytics\pandas\sample_retail_transactions_dataset.csv",
-    parse_dates=["order_date", "ship_date", "signup_date", "last_login"]
-)
+# data = pd.read_csv(
+#     r"C:\Users\Kelvin\Documents\Sereti\Data-analytics\pandas\",
+#     parse_dates=["order_date", "ship_date", "signup_date", "last_login"]
+# )
