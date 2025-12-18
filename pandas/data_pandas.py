7a9fb5f1b510d43f1050950d3eaffa525bd3864a
dev-sereti <devsereti@gmail.com>
2025-12-18 12:26:03 +0300

    Null values

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -16,6 +16,6 @@ print(data.columns)
 print(data.dtypes)
 
 #null values
-
+nulls = data.isna().sum().sort(ascending = False)
 
 # df["transaction_id"].nunique(), df["order_id"].nunique(), df["customer_id"].nunique()
