80a7b6d983bee419c475b47d965cd4847358939b
dev-sereti <devsereti@gmail.com>
2025-12-18 12:07:39 +0300

    Parse dates

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -5,3 +5,5 @@ data = pd.read_csv( r"C:\Users\Kelvin\Documents\Sereti\Data-analytics\pandas\Dat
 print(data.head())
 #Parse dates
 date_cols = ["signup_date","last_login","order_date","ship_date"]
+#Boolens 
+bool_cols = ["returned", "is_vip", "duplicate_flag"]
