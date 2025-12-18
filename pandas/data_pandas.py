9452343589b5d81830ad263cc535901ddf01a893
dev-sereti <devsereti@gmail.com>
2025-12-18 11:55:30 +0300

    Parse dates

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # Load data
 data = pd.read_csv( r"C:\Users\Kelvin\Documents\Sereti\Data-analytics\pandas\Datasets\Retail Transactions.csv")
-
+print(data.head(10))
 #Parse dates
-
+date_cols = ["signup_date","last_login","order_date","ship_date"]
