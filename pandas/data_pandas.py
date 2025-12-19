612fe005cac49d283b5d738aedb4ba7a47398358
dev-sereti <devsereti@gmail.com>
2025-12-19 14:16:42 +0300

    Add Monetary features (KPIs)

--- pandas/data_pandas.py
+++ pandas/data_pandas.py
@@ -37,12 +37,10 @@ for non_numeric in to_numeric:
     data[non_numeric] = pd.to_numeric(data[non_numeric], errors="coerce")
 
 # Ensure categoricals
-
 categorical_cols = [ "gender","country","channel","payment_method","currency","product_category","promo_code","notes"]
 for cat in categorical_cols:
     data[cat] = data[cat].astype("category")
 
 # Monetary features
-
 data["gross_amount"] = data["quantity"] * data["unit_price"]
 data["discount_amount"] = data["gross_amount"] * data["discount_rate"].clip(lower=0)
